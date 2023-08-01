2ff1452e10b11266fdb3fec010b0124d51712246
Alexander Morugin <nobilis@bk.ru>
2023-08-01 14:37:18 +0300

    made fibonacci

---
+++ src/components/fibonacci-page/utils.ts
@@ -0,0 +1,14 @@
+  // Алгоритм итеративного решения
+  export const fibIterative = (n: number): any => {
+    if (n < 1 || n > 19) {
+      return;
+    }
+
+    let array: number[] = [1, 1];
+
+    for (let i = 2; i <= n; i++) {
+      array.push(array[i - 2] + array[i - 1]);
+    }
+
+    return array;
+  };
