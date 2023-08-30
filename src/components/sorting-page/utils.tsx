76f3d443a27a36ecea9b90d36d55c9e489ceeb4b
Alexander Morugin <nobilis@bk.ru>
2023-08-30 15:55:29 +0300

    made sorting unit test

--- src/components/sorting-page/utils.tsx
+++ src/components/sorting-page/utils.tsx
@@ -1,3 +1,4 @@
+import { Direction } from "../../types/direction";
 import { ElementStates } from "../../types/element-states";
 
 export enum ArrayData {
@@ -18,3 +19,35 @@ export const sorting = (array: Array<ISort>, prev: number, next: number): Array<
   array[next].value = temp;
   return array;
 };
+
+// testing
+
+export const testSorting = (array: Array<number>, prev: number, next: number) => {
+  return ([array[prev], array[next]] = [array[next], array[prev]]);
+};
+
+export const bubbleSortTest = (array: Array<number>, direction: Direction) => {
+  const length = array.length;
+  for (let i = 0; i < length - 1; i++) {
+    for (let j = 0; j < length - i - 1; j++) {
+      if (direction === Direction.Ascending ? array[j] > array[j + 1] : array[j] < array[j + 1]) {
+        testSorting(array, j, j + 1);
+      }
+    }
+  }
+  return array;
+};
+
+export const selectionSortTest = (array: Array<number>, direction: Direction) => {
+  const length = array.length;
+  for (let i = 0; i < length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < length; j++) {
+      if (direction === Direction.Ascending ? array[j] < array[minIndex] : array[j] > array[minIndex]) {
+        minIndex = j;
+      }
+    }
+    testSorting(array, minIndex, i);
+  }
+  return array;
+  };
